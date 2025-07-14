2ea90ad288987644f7da87be819f778cc13d2e29
HellDads <patres.inferni@gmail.com>
2025-07-14 16:49:41 +0200

    refactor daily quote from js to json

--- quotes/daily.js
+++
@@ -1,4 +0,0 @@
-export const quoteOfTheDay = {
-  author: "Service Technician",
-  quote: "Helldiver! Every day, you put yourself at risk to defend Freedom and Liberty, and I just wanna say... Thank you."
-};
